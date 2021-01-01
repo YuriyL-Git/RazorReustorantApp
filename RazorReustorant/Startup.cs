77d84ba7e2be7f6c31b52d02528ff4c8ae96b134
Yuriy <xaosmmm@gmail.com>
2021-01-01 19:18:16 +0200

    Added Console app

--- RazorReustorant/Startup.cs
+++ RazorReustorant/Startup.cs
@@ -8,6 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLibrary;
+using DataLibrary.Data;
+using DataLibrary.Db;
 
 namespace RazorReustorant
 {
@@ -23,7 +26,12 @@ namespace RazorReustorant
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+           
+            services.AddSingleton(new ConnectionStringData {SqlConnectionName = "Default"});
+            services.AddSingleton<IDataAccess, SqlDB>();
+            services.AddSingleton<IFoodData, FoodData>();
+            services.AddSingleton<IOrderData, OrderData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
