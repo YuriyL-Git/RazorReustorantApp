cf5f21b88546238503e2685e3d3c2075978d9687
Yuriy <xaosmmm@gmail.com>
2021-01-01 20:12:28 +0200

    Added get fucntional, not working

---
+++ TestApp/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace TestApp
+{
+    public static class Config
+    {
+      
+    }
+}
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using DataLibrary.Data;
+using DataLibrary.Db;
+using DataLibrary.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace TestApp
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            GetFood();
+        }
+
+
+
+        public static async Task GetFood()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            IConfiguration config = builder.Build();
+            ConnectionStringData connectionString = new ConnectionStringData();
+
+            IDataAccess db = new SqlDB(config);
+            IFoodData foodData = new FoodData(db, connectionString);
+
+            var listOfFood =  await foodData.GetFood();
+
+            int x = 5;
+            foreach (var food in listOfFood)
+            {
+                Console.WriteLine(food.Title);
+            }
+          
+
+
         }
     }
 }
